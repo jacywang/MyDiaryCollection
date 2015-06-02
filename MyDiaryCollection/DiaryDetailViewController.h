d44b72b79dadf146bce5f644627ee8ae197346c6
Jacy Wang <jacywj@gmail.com>
2015-06-02 16:34:35 -0700

    setup DiaryDetailViewController

---
+++ MyDiaryCollection/DiaryDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DiaryDetailViewController.h
+//  MyDiaryCollection
+//
+//  Created by JIAN WANG on 6/2/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiaryDetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *diaryImageView;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UITextView *diaryTextView;
+
+@end
